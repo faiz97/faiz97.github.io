039e3fc5882247037153353f965544dac4831e1f
faiz97 <maulafaiz97@gmail.com>
2018-04-29 20:16:22 +0700

    Implementing square, board, and game with props

--- src/App.js
+++ src/App.js
@@ -13,9 +13,78 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Game />
       </div>
     );
   }
 }
+
+
+class Square extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+
+class Board extends Component {
+  renderSquare(i) {
+    return(<Square value={i}/>);
+  }
+
+  render() {
+    const status = 'Next Player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>  
+      </div>
+    );
+  }
+}
+
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default App;
